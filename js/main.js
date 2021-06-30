43fa10b1905fdcad3781a68eca0c6b6e6be4dfd1
islom929 <islomsultonov0@gmail.com>
2021-07-01 00:07:38 +0500

    initialize commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var elSiteHeader = document.querySelector(".header")
+
+var elSiteHeaderToggle = elSiteHeader.querySelector(".header__button")
+
+if(elSiteHeaderToggle){
+  elSiteHeaderToggle.addEventListener("click", function(){
+    elSiteHeader.classList.toggle("header--open")
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
