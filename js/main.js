b5102ae4853e9d5f5cf5faa2a217a759e91461cb
islom929 <islomsultonov0@gmail.com>
2021-07-09 22:35:29 +0500

    smth changed

--- js/main.js
+++ js/main.js
@@ -10,6 +10,48 @@ if(elSiteHeaderToggle){
 
 
 
+var elSiteInfo = document.querySelectorAll(".info__box")
+
+var elSiteInfoToggle = elSiteInfo.querySelector(".info__button")
+
+if(elSiteInfoToggle){
+  elSiteInfoToggle.addEventListener("click", function(){
+    elSiteInfo.classList.toggle("info__box--active")
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
